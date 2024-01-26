9f23a0bc4581d078b412bf462ce185f8c96df9bf
Marc Langer <marc.jentsch@hsh-berlin.com>
2024-01-26 15:09:00 +0100

    ...

--- src/example_notes/examples.ts
+++ src/example_notes/examples.ts
@@ -1,7 +1,21 @@
-// @ts-nocheck 
-// import daily from "./notes/Daily Note.md";
-// import template from "./notes/Template Note.md";
+import address from "./notes/address.md";
+import application from "./notes/application.md";
+import band from "./notes/band.md";
+import book from "./notes/book.md";
+import codeSnippet from "./notes/code snippet.md";
+import dailyNote from "./notes/daily note.md";
+import meeting from "./notes/meeting.md";
+import movie from "./notes/movie.md";
+import musicAlbum from "./notes/music album.md";
+import person from "./notes/person.md";
+import place from "./notes/place.md";
+import poem from "./notes/poem.md";
+import projectRoot from "./notes/project root.md";
+import recipe from "./notes/recipe.md";
 import simpleNote from "./notes/simple note.md";
+import song from "./notes/song.md";
+import template from "./notes/template.md";
+import tvSeries from "./notes/tv series.md";
 
 // // ---------------------
 
@@ -12,20 +26,37 @@ import html from "./snippets/html.md";
 import mermaid from "./snippets/mermaid.md";
 import table from "./snippets/table.md";
 import phone from "./snippets/phone.md";
+import dataview from "./snippets/dataview.md";
 
 export const examples: { [key: string]: { [key: string]: string } } = {
 	notes: {
-		// "daily note": daily,
+		address,
+		application,
+		band,
+		book,
+		"code snippet": codeSnippet,
+		"daily note": dailyNote,
+		meeting,
+		movie,
+		"music album": musicAlbum,
+		person,
+		place,
+		poem,
+		"project root": projectRoot,
+		recipe,
 		"simple note": simpleNote,
-		// "template note": template,
+		song,
+		"template note": template,
+		"tv series": tvSeries
 	},
 	snippets: {
 		callout,
-		codefence,
 		checkbox,
+		codefence,
+		dataview,
 		html,
 		mermaid,
+		phone,
 		table,
-		phone
 	},
 };
